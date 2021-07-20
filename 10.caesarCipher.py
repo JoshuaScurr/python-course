98b384edd2c96cbe6ab3a5a31a878f1cd33d7f30
Joshua Scurr <Joshua Scurr>
2021-07-20 21:37:12 +0100

    10

---
+++ 10.caesarCipher.py
@@ -0,0 +1,17 @@
+# CAESAR CIPHER!
+# your first big challenge
+
+# I want you to build me a Caesar Cipher - this was one of the things from my GCSE coursework, and is the task that gave me the biggest boost in learning programming; it might be quite a challenge, but you have all the tools you need now, and most of the course so far has been specifically leading to this moment
+
+# how a Caesar Cipher works:
+# each letter is shifted by a certain number of places, the offset, wrapping around if you go past Z
+# for example, using 1 as an offset, "abcxyz" becomes "bcdyza" when encrypting
+# and decrypting "bcdyza" with an offset of 1 returns you back to "abcxyz"
+# non-alphabetical characters are unaffected by this version of the cipher
+
+# to be specific, I want you to write a program that first asks the user if they want to encrypt or decrypt, then asks them for the offset to use
+# they then provide the message to encrypt/decrypt, and the program then provides the correctly encrypted/decrypted message
+# once it's done, try to do some thorough testing, eg. testing edge cases, invalid input, etc., and also make sure the program is nice and user-friendly
+
+# if you're stuck with things, take a day or two off - this isn't going to just take an afternoon - it might well take quite a while to get to grips with it - it certainly did for me
+# once you're sure about the code, we can review it together, to see where you might be able to make it neater, etc. (I'll look for my old GCSE one, which was a mess, but I think it might be lost for good)
